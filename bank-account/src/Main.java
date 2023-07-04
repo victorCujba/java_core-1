cea92593cd59839bbd088776218cb214e856a998
Davide Mastroianni <davide.mastroianni@euris.it>
2023-07-04 10:29:26 +0200

    Uploaded exercise back account

---
+++ bank-account/src/Main.java
@@ -0,0 +1,23 @@
+/*
+   ESERCITAZIONE
+
+   PARTE 1:
+       Creare un programma per la gestione di un conto corrente.
+       Il programma deve essere in grado di effettuare le seguenti operazioni da lista di comando.
+           - versamenti
+           - prelievi
+           - restituire il saldo
+           - effettuare la lista degli ultimi 5 movimenti
+       Il programma deve essere inizializzato nella classe main con almeno 1 conto corrente salvato.
+
+   PARTE 2:
+       Fare in modo che il programma permetta da linea di comando di gestire gli utenti:
+           - vedere la lista di tutti gli utenti registrati
+           - inserire un nuovo utente
+           - selezionare un utente in modo da effettuare le operazioni sul conto corrente
+*/
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
