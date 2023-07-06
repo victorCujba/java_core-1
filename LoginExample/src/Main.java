db73615cba0b42433b103f82e85586904770d0f5
Davide Mastroianni <davide.mastroianni@euris.it>
2023-07-06 10:10:40 +0200

    first commit exercise Login

---
+++ LoginExample/src/Main.java
@@ -0,0 +1,21 @@
+
+
+/*
+    modellare 3 tipi di utenti:
+        - admin
+        - editor
+        - user
+    Un utente incapsula gli attributi "username" e "password"
+
+    questi utenti sono inseriti all'interno di un database rappresentato dalla classe ApplicationUsers;
+    il database va rappresentato da una struttura dati adeguata (List, Set, Map)
+    La classe ApplicationUsers mi permette di verificare se la coppia <username, password> esiste ed é corretta.
+    Se esiste allora ritorno il corrispondente utente.
+    La classe ApplicationUsers mi permette di creare un utente dati uno username, password e ruolo.
+
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
