56c2eb49d4fe6d0313a6a996be702f85e3e1ca52
Davide Mastroianni <davide.mastroianni@euris.it>
2023-07-13 15:08:58 +0200

    Created zoo exercise

---
+++ zoo-manager/src/Main.java
@@ -0,0 +1,28 @@
+
+/*
+    Creare un programma Java che possa gestire una serie di giardini zoologici
+
+    Ogni parco deve poter contenere:
+    -orario di apertura
+    -numero di animali max (capienza)
+    -deve indicare quali sezioni/spazi sono presenti fra questi 3:
+        - RecintoAcquatico (può contenere solo animali acquatici)
+        - RecintoVolatili (può contenere solo volatili)
+        - RecintoStandard (può contenere il resto degli animali)
+    -ogni sezione/spazio deve specificare:
+        -lista degli animali presenti
+        -orario di apertura del recinto (OGNI ZOO DEVE AVERE ORARI DIVERSI)
+
+    Il programma  deve consentire ad un utente di consultare il registro di questi parchi per:
+    1)	Cercare quali zoo contengono un certo tipo di animale
+    2)	Selezionare uno zoo per capire se è aperto in questo momento
+    3)	Cercare in modo specifico un certo animale in un certo zoo
+    4)	Richiedere la possibilità di visitare un certo animale in un certo zoo all’interno di un recinto in questo momento
+    5)	Stampare lista degli animali presenti in un certo zoo
+    6)	Stampare lista degli animali presenti in un certo zoo per un certo sezione/spazio richiesto
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
